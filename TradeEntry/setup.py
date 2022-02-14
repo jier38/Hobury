a2b7147d8b7752e891d88893f63893b9b3799088
Long Radix <74498097+longradix@users.noreply.github.com>
2022-02-14 21:21:05 +0100

    Classifiers, description and coding conventions

--- TradeEntry/setup.py
+++ TradeEntry/setup.py
@@ -2,25 +2,30 @@
 
 from setuptools import setup
 
-setup(name='Trade',
-      version='2.0',
-      packages=['trade'],
-      author='Hobury Investments',
-      author_email='',
-      keywords='trac trade',
-      description='',
-      url='',
-      license='BSD',
-      zip_safe=False,
-      extras_require={},
-      entry_points={'trac.plugins': [
+setup(name = 'Trade',
+      version = '2.0',
+      packages = ['trade'],
+      author = 'Hobury Investments',
+      author_email = '',
+      keywords = 'trac trade',
+      description = 'A trade entry system and trade overview within Trac',
+      url = '',
+      license = 'BSD',
+      install_requires = ['Trac >= 1.4.1'],
+      zip_safe = False,
+      extras_require = {},
+      entry_points = {'trac.plugins': [
           'trade.web_ui = trade.web_ui']},
-      package_data={'trade': ['htdocs/*.png',
+      package_data = {'trade': ['htdocs/*.png',
                               'htdocs/css/*.css',
                               'htdocs/js/*.js',
-                              'templates/*.html',
-                              'templates/*.rss', ]},
-      exclude_package_data={},
-      test_suite='',
-      tests_require=[],
-      install_requires=[])
+                              'templates/*.html']},
+      classifiers = ['Development Status :: 5 - Production/Stable', 
+                     'Framework :: Trac',
+                     'Programming Language :: Python'],
+      exclude_package_data = {},
+      test_suite = '',
+      tests_require = [],
+      install_requires = []
+)
+
