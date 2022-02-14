853a5624e3df4c7a5bc4bade3d4893ae6cfe1f46
Long Radix <74498097+longradix@users.noreply.github.com>
2022-02-14 21:01:04 +0100

    Update setup.py
    Classifiers, description, coding conventions

--- ChartMacro/setup.py
+++ ChartMacro/setup.py
@@ -2,24 +2,26 @@
 
 from setuptools import find_packages, setup
 
-version='0.1'
+version = '0.1'
 
-setup(name='ChartMacro',
-      version=version,
-        description = "A Trac wiki macro that shows charts on a Trac site.",
-      long_description="",
-        author = 'John',
-    author_email = '',
-    url= '',
-    keywords = 'trac plugin',
-    license = "",
-      install_requires = [ 'Trac>=1.4.1' ],
-      packages=find_packages(exclude=['ez_setup', 'examples', '*tests*']),
-      include_package_data=True,
-      package_data={  },
-      zip_safe=True,
-      entry_points={'trac.plugins': [
+setup(name = 'ChartMacro',
+      version = version,
+      description = "Display charts on a Trac site.",
+      long_description = "",
+      author = 'Hobury Investments',
+      author_email = '',
+      url = '',
+      keywords = 'trac plugin',
+      license = "BSD",
+      install_requires = ['Trac >= 1.4.1'],
+      packages = find_packages(exclude = ['ez_setup', 'examples', '*tests*']),
+      include_package_data = True,
+      package_data = {  },
+      zip_safe = True,
+      entry_points = {'trac.plugins': [
             'chartmacro.api = chartmacro.api'
             ]},
-      classifiers=[],
-      )
+      classifiers = ['Development Status :: 5 - Production/Stable', 
+                     'Framework :: Trac',
+                     'Programming Language :: Python']
+)
