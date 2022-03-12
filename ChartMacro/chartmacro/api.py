aad512be3cff1528ccc3ebd90ca3a83cbc967b29
jier38 <jier38@gmail.com>
2022-03-12 10:12:41 +0800

    migrate from jqplot to plotly

--- ChartMacro/chartmacro/api.py
+++ ChartMacro/chartmacro/api.py
@@ -10,7 +10,7 @@ from trac.web.chrome import add_script, add_stylesheet
 class ChartMacro(WikiMacroBase):
     implements(IRequestFilter)
 	
-	# [[Chart(TicketStatistic)]]
+	# [[Chart(TicketStatistics)]]
     def expand_macro(self, formatter, name, content):
         chart_type = ""
         buf = ""
@@ -31,22 +31,27 @@ class ChartMacro(WikiMacroBase):
                 closedtickets += str(row[2]) + ',' 
                 tick = row[0].split(' ')
                 ticks += '"' + tick[len(tick)-1] + '",' 
-            buf += '<div id="ticketschart" style="width: 500px; height: 300px;"></div>\n'
-            buf += '<script type="text/javascript">\n'
-            buf += '$(document).ready(function() { \n' 
-            buf +=     '  var opentickets = [' + opentickets + ']; \n' 
-            buf +=    '  var closedtickets = [' + closedtickets + ']; \n' 
-            buf +=    '  var ticks = [' + ticks + ']; \n'  		 
-            buf +=    '  var ticketsplot = $.jqplot("ticketschart", [opentickets, closedtickets], { \n' 
-            buf +=   '    series: [{label: "Open", renderer:$.jqplot.BarRenderer}, {label: "Closed", xaxis: "xaxis", yaxis: "yaxis"}], \n' 
-            buf +=    '    title: "Tickets per milestone", \n' 
-            buf +=     '    highlighter: {show: true}, grid: {background: "#fff"}, \n' 
-            buf +=      '    axesDefaults: {tickRenderer: $.jqplot.CanvasAxisTickRenderer}, \n' 
-            buf +=        '    axes: {xaxis: {renderer: $.jqplot.CategoryAxisRenderer, ticks: ticks, tickOptions: {showGridline: false}}, yaxis: {autoscale: true}}, \n' 
-            buf +=    '    legend: {show: true, location: "e", placement: "outside"} \n' 
-            buf +=      '  }); \n' 
-            buf +=     '}); \n'
-            buf += '</script>\n'
+            buf += '<div id="ticketschart" style="width: 800px; height: 500px;"></div> \n'
+            buf += '<script type="text/javascript"> \n'         
+            buf += 'CHART = document.getElementById("ticketschart"); \n'
+            buf += 'var trace1 = { \n'
+            buf += '  x: [' + ticks + '], \n'
+            buf += '  y: [' + opentickets + '], \n'
+            buf += '  type: "bar", \n'
+            buf += '  name: "Open", \n'
+            buf += '}; \n'
+            buf += 'var trace2 = { \n'
+            buf += '  x: [' + ticks + '], \n'
+            buf += '  y: [' + closedtickets + '], \n'
+            buf += '  mode: "lines+markers", \n'
+            buf += '  name: "Closed", \n'
+            buf += '  marker: { size: 8 }, \n'
+            buf += '  line: { width: 0.8 } \n'
+            buf += '}; \n'
+            buf += 'var data = [ trace1, trace2 ]; \n'
+            buf += 'var layout = { title:"Tickets per milestone" }; \n'
+            buf += 'Plotly.newPlot(CHART, data, layout); \n'
+            buf += '</script> \n'
         elif (chart_type == 'Statistics'):
             cursor = self.env.db_query(
                 "SELECT exchange, indicator, k, lambda, weightfunction, mRS_buy, mRS_sell, holdingperiod, rel_profit, win_ratio from invest.statistics;"
@@ -79,20 +84,11 @@ class ChartMacro(WikiMacroBase):
 		
     # IRequestFilter#post_process_request
     def post_process_request(self, req, template, data, content_type):
-        plugin_base = 'https://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.9/'
-        add_script(req, plugin_base + 'jquery.jqplot.min.js')
-        add_script(req, plugin_base + 'jqplot.dateAxisRenderer.js')
-        add_script(req, plugin_base + 'jqplot.canvasTextRenderer.js')
-        add_script(req, plugin_base + 'jqplot.canvasAxisTickRenderer.js')
-        add_script(req, plugin_base + 'jqplot.categoryAxisRenderer.js')
-        add_script(req, plugin_base + 'jqplot.barRenderer.js')
-        add_script(req, plugin_base + 'jqplot.highlighter.min.js')
-        add_stylesheet(req, 'https://cdnjs.cloudflare.com/ajax/libs/jqPlot/1.0.9/jquery.jqplot.min.css')
-        #add_script(req, 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js')		
-        #add_script(req, 'https://cdnjs.cloudflare.com/ajax/libs/jquery-csv/1.0.5/jquery.csv.js')
-        #add_script(req, 'https://pivottable.js.org/dist/pivot.js')
-        #add_script(req, 'https://cdn.plot.ly/plotly-basic-latest.min.js')
-        #add_script(req, 'https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.23.0/plotly_renderers.min.js')
-        #add_script(req, 'https://d3js.org/d3.v5.min.js')
-        #add_stylesheet(req, 'https://pivottable.js.org/dist/pivot.css')
+        add_script(req, 'https://cdnjs.cloudflare.com/ajax/libs/jquery-csv/1.0.5/jquery.csv.js')
+        add_script(req, 'https://pivottable.js.org/dist/pivot.js')
+        add_script(req, 'https://cdn.plot.ly/plotly-basic-latest.min.js')
+        add_script(req, 'https://cdnjs.cloudflare.com/ajax/libs/pivottable/2.23.0/plotly_renderers.min.js')
+        add_script(req, 'https://d3js.org/d3.v5.min.js')
+        add_stylesheet(req, 'https://pivottable.js.org/dist/pivot.css')
+        add_script(req, 'https://cdn.plot.ly/plotly-2.9.0.min.js')
         return (template, data, content_type)		
